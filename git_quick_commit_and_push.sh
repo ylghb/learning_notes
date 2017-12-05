fba0017437466cf7e2c01298c89bfe788cfeb059
Luo Hongshuai <lhsfboy@gmail.com>
2017-12-05 22:02:27 +0900

    2017-12-05	22:02:27 Daily Commit

--- git_quick_commit_and_push.sh
+++ git_quick_commit_and_push.sh
@@ -1,4 +1,4 @@
-git add -A; git commit -m "`date +"%F%t%T"` Daily Commit";git push;
+git pull;git add -A; git commit -m "`date +"%F%t%T"` Daily Commit";git push;
 
 # run this script on windows gitbash need to use sh command
 # eg: sh git_push.sh
