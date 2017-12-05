ce5c382178f92048e7f021cc2816628a65e73b67
Luo Hongshuai <lhsfboy@gmail.com>
2017-12-05 22:00:36 +0900

    2017-12-05	22:00:36 Daily Commit

---
+++ Asset_Pricing/cleanup.sh
@@ -0,0 +1,221 @@
+## Core latex/pdflatex auxiliary files:
+rm -f *.aux
+rm -f *.lof
+rm -f *.log
+rm -f *.lot
+rm -f *.fls
+rm -f *.out
+rm -f *.toc
+rm -f *.fmt
+rm -f *.fot
+rm -f *.cb
+rm -f *.cb2
+
+## Intermediate documents:
+rm -f  *.dvi
+rm -f  *.xdv
+rm -f  *-converted-to.*
+# these rules might exclude image files for figures etc.
+# *.ps
+# *.eps
+# *.pdf
+
+## Generated if empty string is given at "Please type another file name for output:"
+rm -f  .pdf
+
+## Bibliography auxiliary files (bibtex/biblatex/biber):
+rm -f *.bbl
+rm -f *.bcf
+rm -f *.blg
+rm -f *-blx.aux
+rm -f *-blx.bib
+rm -f *.run.xml
+
+## Build tool auxiliary files:
+rm -f *.fdb_latexmk
+rm -f *.synctex
+rm -f *.synctex\(busy\)
+rm -f *.synctex.gz
+rm -f *.synctex.gz\(busy\)
+rm -f *.pdfsync
+
+## Auxiliary and intermediate files from other packages:
+# algorithms
+rm -f *.alg
+rm -f *.loa
+
+# achemso
+rm -f acs-*.bib
+
+# amsthm
+rm -f *.thm
+
+# beamer
+rm -f *.nav
+rm -f *.pre
+rm -f *.snm
+rm -f *.vrb
+
+# changes
+*.soc
+
+# cprotect
+*.cpt
+
+# elsarticle (documentclass of Elsevier journals)
+*.spl
+
+# endnotes
+*.ent
+
+# fixme
+*.lox
+
+# feynmf/feynmp
+*.mf
+*.mp
+*.t[1-9]
+*.t[1-9][0-9]
+*.tfm
+
+#(r)(e)ledmac/(r)(e)ledpar
+*.end
+*.?end
+*.[1-9]
+*.[1-9][0-9]
+*.[1-9][0-9][0-9]
+*.[1-9]R
+*.[1-9][0-9]R
+*.[1-9][0-9][0-9]R
+*.eledsec[1-9]
+*.eledsec[1-9]R
+*.eledsec[1-9][0-9]
+*.eledsec[1-9][0-9]R
+*.eledsec[1-9][0-9][0-9]
+*.eledsec[1-9][0-9][0-9]R
+
+# glossaries
+*.acn
+*.acr
+*.glg
+*.glo
+*.gls
+*.glsdefs
+
+# gnuplottex
+*-gnuplottex-*
+
+# gregoriotex
+*.gaux
+*.gtex
+
+# hyperref
+*.brf
+
+# knitr
+*-concordance.tex
+# TODO Comment the next line if you want to keep your tikz graphics files
+*.tikz
+*-tikzDictionary
+
+# listings
+*.lol
+
+# makeidx
+*.idx
+*.ilg
+*.ind
+*.ist
+
+# minitoc
+*.maf
+*.mlf
+*.mlt
+*.mtc[0-9]*
+*.slf[0-9]*
+*.slt[0-9]*
+*.stc[0-9]*
+
+# minted
+_minted*
+*.pyg
+
+# morewrites
+*.mw
+
+# nomencl
+*.nlo
+
+# pax
+*.pax
+
+# pdfpcnotes
+*.pdfpc
+
+# sagetex
+*.sagetex.sage
+*.sagetex.py
+*.sagetex.scmd
+
+# scrwfile
+*.wrt
+
+# sympy
+*.sout
+*.sympy
+sympy-plots-for-*.tex/
+
+# pdfcomment
+*.upa
+*.upb
+
+# pythontex
+*.pytxcode
+pythontex-files-*/
+
+# thmtools
+*.loe
+
+# TikZ & PGF
+*.dpth
+*.md5
+*.auxlock
+
+# todonotes
+*.tdo
+
+# easy-todo
+*.lod
+
+# xindy
+*.xdy
+
+# xypic precompiled matrices
+*.xyc
+
+# endfloat
+*.ttt
+*.fff
+
+# Latexian
+TSWLatexianTemp*
+
+## Editors:
+# WinEdt
+*.bak
+*.sav
+
+# Texpad
+.texpadtmp
+
+# Kile
+*.backup
+
+# KBibTeX
+*~[0-9]*
+
+# auto folder when using emacs and auctex
+/auto/*
+
+# expex forward references with \gathertags
+*-tags.tex
